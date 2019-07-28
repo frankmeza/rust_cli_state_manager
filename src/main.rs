9a055caa78b6735a2c7fadb96bb7de27c9be13b0
Frank Meza <frankmezapdx@gmail.com>
2019-07-27 22:35:06 -0700

    play with cli args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use std::env;
+
+#[derive(Debug)]
 enum Mutation {
     ChangeEditedBy(String),
+    Nothing(),
 }
 
 #[derive(Debug)]
@@ -30,6 +34,7 @@ impl AppState {
             Mutation::ChangeEditedBy(edited_by) => AppState {
                 edited_by: edited_by.to_string(), ..current_state
             },
+            Mutation::Nothing() => AppState { ..current_state }
         }
     }
 
@@ -39,11 +44,33 @@ impl AppState {
             Mutation::ChangeEditedBy(String::from("edited_by")),
         ]
     }
+
+    fn create_mutation(mutation_type: &String, mutation_value: &String) -> Mutation {
+        let is_change_edited_by = mutation_type == "CHANGE_EDITED_BY";
+
+        match is_change_edited_by {
+             true => {
+                Mutation::ChangeEditedBy(mutation_value.to_string())
+            },
+            false => {
+                Mutation::Nothing()
+            }
+        }
+    }
 }
 
 fn main() {
     let state = AppState::new();
-    println!("{:?}", state);
+    println!("INITIAL_STATE IS {:?}", state);
+
+    let args: Vec<String> = env::args().collect();
+    let mutation_type = &args[1].clone();
+    let mutation_value = &args[2].clone();
+
+    let new_mutation = AppState::create_mutation(mutation_type, mutation_value);
+
+    println!("{:?}", new_mutation);
+    println!("{}: {}", mutation_type, mutation_value);
 
     let mutations = AppState::init_mutations();
 
