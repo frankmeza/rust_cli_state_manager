d393fbc24480c301460cb4202ef33d042fb8370c
Frank Meza <frankmezapdx@gmail.com>
2019-08-12 22:47:40 -0700

    fix a few bugs...

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,25 @@ use std::{
 mod app;
 
 const CHOOSE: &str = "Choose your desired mutation by number: ";
-const CHANGE_EDITED_BY: &str = "1. CHANGE_EDITED_BY";
-const CHANGE_IS_DAYTIME: &str = "2. CHANGE_IS_DAYTIME";
-const CHANGE_COLOR: &str = "3. CHANGE_COLOR";
-const CHANGE_NUMBER_INCREMENT: &str = "4. CHANGE_NUMBER_INCREMENT";
-const CHANGE_NUMBER_DECREMENT: &str = "5. CHANGE_NUMBER_DECREMENT";
+const CHANGE_EDITED_BY: &str = "CHANGE_EDITED_BY";
+const CHANGE_IS_DAYTIME: &str = "CHANGE_IS_DAYTIME";
+const CHANGE_COLOR: &str = "CHANGE_COLOR";
+const CHANGE_NUMBER_INCREMENT: &str = "CHANGE_NUMBER_INCREMENT";
+const CHANGE_NUMBER_DECREMENT: &str = "CHANGE_NUMBER_DECREMENT";
 
 fn print_mutation_types() -> () {
     let prompts = vec![
-        "Choose your desired mutation by number: ",
-        "1. CHANGE_EDITED_BY",
-        "2. CHANGE_IS_DAYTIME",
-        "3. CHANGE_COLOR",
-        "4. CHANGE_NUMBER_INCREMENT",
-        "5. CHANGE_NUMBER_DECREMENT",
+        CHOOSE,
+        "press 1 for :",
+        CHANGE_EDITED_BY,
+        "press 2 for :",
+        CHANGE_IS_DAYTIME,
+        "press 3 for :",
+        CHANGE_COLOR,
+        "press 4 for :",
+        CHANGE_NUMBER_INCREMENT,
+        "press 5 for :",
+        CHANGE_NUMBER_DECREMENT,
     ];
 
     for p in &prompts {
@@ -26,32 +31,28 @@ fn print_mutation_types() -> () {
     }
 }
 
-fn get_mutation_type(mtype: &String) -> &str {
-    match mtype.as_ref() {
-        // fn usize(number: &str) -> usize { number.trim().pa}
+fn get_mutation_type(mtype: &str) -> &str {
+    match mtype {
         "1" => {
-            CHOOSE
-        }
-        "2" => {
             CHANGE_EDITED_BY
         }
-        "3" => {
+        "2" => {
             CHANGE_IS_DAYTIME
         }
-        "4" => {
+        "3" => {
             CHANGE_COLOR
         }
-        "5" => {
+        "4" => {
             CHANGE_NUMBER_INCREMENT
         }
-        "6" => {
+        "5" => {
             CHANGE_NUMBER_DECREMENT
         }
         "EXIT" => {
             "byeeee"
         }
         _ => {
-            ""
+            "very virus"
         }
     }
 }
@@ -60,22 +61,27 @@ fn main() {
     let state = app::AppState::new();
 
     println!("\n INITIAL_STATE IS {:?}\n", state);
-
     print_mutation_types();
-    // creates receiver variable
-    let mut choice = String::new();
 
-    let mtype: String = io::stdin().read_line(&mut choice)
+    // creates receiver variable
+    let mut c1 = String::new();
+    io::stdin().read_line(&mut c1)
         .unwrap()
         .to_string();
 
-    let mutation_type: &str = get_mutation_type(&mtype);
+    let choice_type: &str = c1.as_str().trim();
+    let mutation_type: &str = get_mutation_type(&choice_type);
 
     println!("You chose {}.\nNow choose your desired mutation: ", mutation_type);
 
-    let mutation_value: String = io::stdin().read_line(&mut choice)
+    // creates a second receiver variable
+    let mut c2 = String::new();
+    io::stdin().read_line(&mut c2)
         .unwrap()
         .to_string();
+
+    let mutation_value: &str = c2.as_str().trim();
+    println!("mutation value : {}", mutation_value);
 
     let new_mutation = app::AppState::create_mutation(
         &mutation_type,
