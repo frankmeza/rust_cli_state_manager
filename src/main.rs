b3155ef5bfa3160eee6b70b49d4d2db7dcc2e388
Frank Meza <frankmezapdx@gmail.com>
2019-08-21 21:43:36 -0700

    "hello ws!"

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
-use std::io;
+use std::{process, io};
+
+extern crate ws;
+use ws::{
+    connect,
+    // listen,
+    CloseCode,
+};
 
 mod app;
 use app::utils;
 
+fn send_ws_msg(mtype: &str, mvalue: &str) -> () {
+    connect("ws://127.0.0.1:3012", |out| {
+        let send_type = format!("TYPE: {}", mtype);
+        let send_value = format!("VALUE: {}", mvalue);
+
+        out.send(send_type).unwrap();
+        out.send(send_value).unwrap();
+
+        move |_msg| {
+            out.close(CloseCode::Normal)
+        }
+    }).unwrap()
+}
+
 fn main() {
     let mut state = app::AppState::new();
-    println!("\n INITIAL_STATE IS {:?}\n", state);
 
     loop {
+        println!("\n INITIAL_STATE IS {:?}\n", state);
+        let s = &state;
+        println!("\n S_STATE IS {:?}\n", s);
+
         utils::print_mutation_types();
 
         // creates receiver variable for type
@@ -18,6 +42,10 @@ fn main() {
 
         let choice_type: &str = c1.as_str().trim();
         let mutation_type: &str = utils::get_mutation_type(&choice_type);
+
+        if mutation_type == "EXIT" || mutation_type == "ERROR" {
+            process::exit(1);
+        }
 
         println!("You chose {}.\nNow choose your desired mutation: ", mutation_type);
 
@@ -43,7 +71,8 @@ fn main() {
         );
 
         state = updated_state;
-
         println!("\n UPDATED STATE IS {:?}\n", state);
+
+        send_ws_msg(mutation_type, mutation_value);
     }
 }
